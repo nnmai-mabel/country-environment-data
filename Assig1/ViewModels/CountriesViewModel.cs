f4973eeb85c0a5dc3b11d148422ad99855a5681f
mainguyen <nguny089@mymail.unisa.edu.au>
2023-08-29 12:14:38 +0930

    feat: add getter-setter properties to countries view model

--- Assig1/ViewModels/CountriesViewModel.cs
+++ Assig1/ViewModels/CountriesViewModel.cs
@@ -3,9 +3,11 @@ namespace Assig1.ViewModels
 {
 	public class CountriesViewModel
 	{
-		public CountriesViewModel()
-		{
-		}
+		public int? RegionId { get; set; }
+		public List<Models.Country> Countries { get; set; }
+		//public CountriesViewModel()
+		//{
+		//}
 	}
 }
 
