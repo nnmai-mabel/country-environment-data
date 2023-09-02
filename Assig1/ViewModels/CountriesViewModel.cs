b0a5af862bb345980b7cf5beda311a6ee8a0d2ec
mainguyen <nguny089@mymail.unisa.edu.au>
2023-09-02 10:13:44 +0930

    feat: add country detail as a list in countries view model

--- Assig1/ViewModels/CountriesViewModel.cs
+++ Assig1/ViewModels/CountriesViewModel.cs
@@ -5,6 +5,7 @@ namespace Assig1.ViewModels
 	{
 		public int? RegionId { get; set; }
 		public List<Models.Country> Countries { get; set; }
+		public Country_CountryDetail CountryList { get; set; }
 		//public CountriesViewModel()
 		//{
 		//}
