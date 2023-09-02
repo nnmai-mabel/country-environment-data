477d99032af88ee94dc9e0aa24c78d1f1cfb3e6d
mainguyen <nguny089@mymail.unisa.edu.au>
2023-09-02 18:10:41 +0930

    feat: change content inside city detail view model

--- Assig1/ViewModels/City_CityDetail.cs
+++ Assig1/ViewModels/City_CityDetail.cs
@@ -3,7 +3,7 @@ namespace Assig1.ViewModels
 {
 	public class City_CityDetail
 	{
-        public List<City_CityDetail> CityList { get; set; }
+        public int? CountryId { get; set; }
         //public City_CityDetail()
         //{
         //}
