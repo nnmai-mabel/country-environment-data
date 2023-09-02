fb1cc38ef06fefe09d320ecc7705dbd497eb6503
mainguyen <nguny089@mymail.unisa.edu.au>
2023-09-02 18:13:11 +0930

    feat: add city to city view model

--- Assig1/ViewModels/City_CityDetail.cs
+++ Assig1/ViewModels/City_CityDetail.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using Assig1.Models;
+
 namespace Assig1.ViewModels
 {
 	public class City_CityDetail
 	{
-        public int? CountryId { get; set; }
+        public City TheCity { get; set; }
         //public City_CityDetail()
         //{
         //}
