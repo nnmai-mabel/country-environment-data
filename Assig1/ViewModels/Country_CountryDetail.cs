a4f7a5d146f88e32772186504ae63d19fca672d2
mainguyen <nguny089@mymail.unisa.edu.au>
2023-09-02 10:10:41 +0930

    feat: add country detail view model

---
+++ Assig1/ViewModels/Country_CountryDetail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Assig1.Models;
+
+namespace Assig1.ViewModels
+{
+	public class Country_CountryDetail
+	{
+		public Country TheCountry { get; set; }
+		//public Country_CountryDetail()
+		//{
+		//}
+	}
+}
+
