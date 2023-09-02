f4390232b73b4665a5bc52eb7cb8d9ba0584827e
mainguyen <nguny089@mymail.unisa.edu.au>
2023-09-02 17:52:10 +0930

    feat: add citites view model

---
+++ Assig1/ViewModels/CitiesViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Assig1.ViewModels
+{
+	public class CitiesViewModel
+	{
+        public int? CountryId { get; set; }
+        //public CitiesViewModel()
+        //{
+        //}
+    }
+}
+
