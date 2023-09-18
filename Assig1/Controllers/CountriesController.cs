70268a79f25809ba1f69783fa36f46b934fe5792
mainguyen <nguny089@mymail.unisa.edu.au>
2023-09-18 21:07:27 +0930

    feat: add group join to show element name on the chart

--- Assig1/Controllers/CountriesController.cs
+++ Assig1/Controllers/CountriesController.cs
@@ -183,6 +183,7 @@ namespace Assig1.Controllers
                     // Calculate average
                     if (vm.ChartAggregation == "Average")
                     {
+                        
                         var countryEmissionsSummary = _context.CountryEmissions
                             .Where(ce => ce.Year == vm.Year)
                             .Where(ce => ce.CountryId == vm.CountryId)
@@ -238,18 +239,52 @@ namespace Assig1.Controllers
                     // Calculate sum
                     else
                     {
-                        var countryEmissionsSummary = _context.CountryEmissions
-                            .Where(ce => ce.Year == vm.Year)
-                            .Where(ce => ce.CountryId == vm.CountryId)
-                            .GroupBy(ce => new { ce.CountryId, ce.ElementId, ce.Year })
+                        var elementEmissions = _context.CountryEmissions
+                            .GroupJoin(_context.Elements,
+                            ce => ce.ElementId,
+                            e => e.ElementId,
+                            (ce, elementGroup) => new
+                            {
+                                theCountryEmission = ce,
+                                theElements = elementGroup
+                            })
+                            .SelectMany(
+                            ce => ce.theElements.DefaultIfEmpty(),
+                            (ce, e) => new
+                            {
+                                theCountryEmission = ce.theCountryEmission,
+                                theElement = e
+                            })
+                            .Where(ce => ce.theCountryEmission.CountryId == vm.CountryId)
+                            .Where(ce => ce.theCountryEmission.Year == vm.Year)
+                            .GroupBy(group => new
+                            {
+                                countryId = group.theCountryEmission.CountryId,
+                                year = group.theCountryEmission.Year,
+                                elementId = group.theCountryEmission.ElementId,
+                                element = group.theElement.ElementName
+                            })
                             .Select(group => new
                             {
-                                countryId = group.Key.CountryId,
-                                year = group.Key.Year,
-                                element = group.Key.ElementId,
-                                valueElement = group.Sum(ce => ce.Value)
+                                countryId = group.Key.countryId,
+                                year = group.Key.year,
+                                elementId = group.Key.elementId,
+                                element = group.Key.element,
+                                valueElement = group.Sum(ce => ce.theCountryEmission.Value)
                             });
-                        return Json(countryEmissionsSummary);
+                        return Json(elementEmissions);
+                        //var countryEmissionsSummary = _context.CountryEmissions
+                        //    .Where(ce => ce.Year == vm.Year)
+                        //    .Where(ce => ce.CountryId == vm.CountryId)
+                        //    .GroupBy(ce => new { ce.CountryId, ce.ElementId, ce.Year })
+                        //    .Select(group => new
+                        //    {
+                        //        countryId = group.Key.CountryId,
+                        //        year = group.Key.Year,
+                        //        element = group.Key.ElementId,
+                        //        valueElement = group.Sum(ce => ce.Value)
+                        //    });
+                        //return Json(countryEmissionsSummary);
                     }
                 }
             }
