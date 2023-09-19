fbbb642c41336776af19495d3ad1d494335fcf15
mainguyen <nguny089@mymail.unisa.edu.au>
2023-09-20 08:26:29 +0930

    feat: add region id to city

--- Assig1/ViewModels/CitiesViewModel.cs
+++ Assig1/ViewModels/CitiesViewModel.cs
@@ -4,6 +4,7 @@ namespace Assig1.ViewModels
 	public class CitiesViewModel
 	{
         public int? CountryId { get; set; }
+        public int? RegionId { get; set; }
         public List<City_CityDetail> CityList { get; set; }
         //public CitiesViewModel()
         //{
