06482db7ca997b62028b6a3650454510b306c814
mainguyen <nguny089@mymail.unisa.edu.au>
2023-09-26 08:52:15 +0930

    refractor: remove unnecessary actions in Regions controller

--- Assig1/Controllers/RegionsController.cs
+++ Assig1/Controllers/RegionsController.cs
@@ -36,24 +36,6 @@ namespace Assig1.Controllers
             return View(RegionList);
         }
 
-        // GET: Regions/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Regions == null)
-            {
-                return NotFound();
-            }
-
-            var region = await _context.Regions
-                .FirstOrDefaultAsync(m => m.RegionId == id);
-            if (region == null)
-            {
-                return NotFound();
-            }
-
-            return View(region);
-        }
-
         private bool RegionExists(int id)
         {
           return (_context.Regions?.Any(e => e.RegionId == id)).GetValueOrDefault();
