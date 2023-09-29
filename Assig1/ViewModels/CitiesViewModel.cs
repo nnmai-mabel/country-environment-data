01866eb11f7656ceb7552eb4dcdfd64b91f4b05b
mainguyen <nguny089@mymail.unisa.edu.au>
2023-09-29 22:30:36 +0930

    feat: add validations and display names

--- Assig1/ViewModels/CitiesViewModel.cs
+++ Assig1/ViewModels/CitiesViewModel.cs
@@ -1,23 +1,50 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
 using Assig1.Models;
 using Microsoft.AspNetCore.Mvc.Rendering;
-
 namespace Assig1.ViewModels
 {
 	public class CitiesViewModel
 	{
+        [Display(Name = "City Search")]
+        [StringLength(100, ErrorMessage = "The {0} must be less than {1} characters")]
         public string? SearchText { get; set; }
+
+        [Display(Name = "Country ID")]
         public int? CountryId { get; set; }
+
+        [Display(Name = "Region ID")]
         public int? RegionId { get; set; }
+
+        [Display(Name = "Country")]
         public Country TheCountry { get; set; }
+
+        [Display(Name = "City ID")]
         public int? CityId { get; set; }
+
+        [Display(Name = "City Detail List")]
         public List<City_CityDetail>? CityDetailList { get; set; }
+
+        [Display(Name = "City Detail")]
         public City_CityDetail? TheCityDetail { get; set; }
+
+        [Display(Name = "Year List")]
         public SelectList? YearList { get; set; }
+
+        [Display(Name = "Year")]
         public int Year { get; set; }
+
+        [Display(Name = "Station Type List")]
         public SelectList? StationTypeList { get; set; }
+
+        [Display(Name = "Station Type")]
         public string? StationType { get; set; }
+
+        [Display(Name = "Chart Legend")]
         public string? ChartLegend { get; set; }
+
+        [Display(Name = "Page Source")]
         public string? PageSource { get; set; }
     }
 }
--- Assig1/ViewModels/CountriesViewModel.cs
+++ Assig1/ViewModels/CountriesViewModel.cs
@@ -1,21 +1,44 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
 using Assig1.Models;
 using Microsoft.AspNetCore.Mvc.Rendering;
-
 namespace Assig1.ViewModels
 {
 	public class CountriesViewModel
 	{
+        [Display(Name = "City Search")]
+        [StringLength(100, ErrorMessage = "The {0} must be less than {1} characters")]
         public string? SearchText { get; set; }
+
+        [Display(Name = "Region ID")]
         public int? RegionId { get; set; }
+
+        [Display(Name = "Country ID")]
         public int? CountryId { get; set; }
+
+        [Display(Name = "Region Select List")]
         public SelectList? RegionSelectList { get; set; }
+
+        [Display(Name = "Countries")]
         public List<Models.Country>? Countries { get; set; }
-		public List<Country_CountryDetail>? CountryList { get; set; }
+
+        [Display(Name = "Country List")]
+        public List<Country_CountryDetail>? CountryList { get; set; }
+
+        [Display(Name = "Country Detail")]
         public Country_CountryDetail? TheCountryDetail { get; set; }
+
+        [Display(Name = "Year List")]
         public SelectList? YearList { get; set; }
+
+        [Display(Name = "Year")]
         public int Year { get; set; }
+
+        [Display(Name = "Chart Legend")]
         public string? ChartLegend { get; set; }
+
+        [Display(Name = "Page Source")]
         public string? ChartAggregation { get; set; }
     }
 }
