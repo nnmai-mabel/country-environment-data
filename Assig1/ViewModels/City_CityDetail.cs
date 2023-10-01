fd5d8b83fb6d38d74c691abb14cc4d52204e754a
mainguyen <nguny089@mymail.unisa.edu.au>
2023-10-01 19:14:42 +1030

    feat: add data annotations/decorators

--- Assig1/ViewModels/City_CityDetail.cs
+++ Assig1/ViewModels/City_CityDetail.cs
@@ -1,15 +1,27 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
 using Assig1.Models;
-
 namespace Assig1.ViewModels
 {
 	public class City_CityDetail
 	{
+        [Display(Name = "City")]
         public City? TheCity { get; set; }
+
+        [Display(Name = "Country")]
         public Country? TheCountry { get; set; }
+
+        [Display(Name = "Region")]
         public Region? TheRegion { get; set; }
+
+        [Display(Name = "Earliest Year of Record")]
         public int? AirMinYear { get; set; }
+
+        [Display(Name = "Latest Year of Record")]
         public int? AirMaxYear { get; set; }
+
+        [Display(Name = "Number of Records")]
         public int? AirRecordCount { get; set; }
     }
 }
--- Assig1/ViewModels/Country_CountryDetail.cs
+++ Assig1/ViewModels/Country_CountryDetail.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
 using Assig1.Models;
 
 namespace Assig1.ViewModels
 {
 	public class Country_CountryDetail
 	{
-		public Country? TheCountry { get; set; }
+        [Display(Name = "Country")]
+        public Country? TheCountry { get; set; }
+
+        [Display(Name = "Region")]
         public Region? TheRegion { get; set; }
     }
 }
